93b8b3db1fc84d291a10aa6783a8a9a962cffe4a
Mukseda Aktar <muksedamukti@gmail.com>
2025-12-07 21:46:15 +0600

    all setup for this project is done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <h2 className='text-blue-600'>Contes Hub</h2>
+      <h4>Contes Hub</h4> */}
+      
+    </>
+  )
+}
+
+export default App
